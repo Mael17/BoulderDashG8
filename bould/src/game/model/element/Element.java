af6e9b513bee7627fb1a97b79374c214ff7af04c
uzielieno <50231474+uzielieno@users.noreply.github.com>
2020-05-22 14:08:29 +0100

    Voici mon premier commit

--- bould/src/game/model/element/Element.java
+++ bould/src/game/model/element/Element.java
@@ -1,80 +1,50 @@
 package game.model.element;
 
-/**
- * Elemento del juego. Celda, item o actor.
- * Tiene posicion y un caracter de identificacion.
- */
+
 public class Element
 {
 	private ElementTypes type;
 	private Position pos;
 
-	/**
-	 * Constructor de elementos.
-	 * 
-	 * @param pos
-	 */
+	
 	public Element(Position pos)
 	{
 		super();
 		this.pos = pos;
 	}
 
-	/**
-	 * Retorna la representacion del elemento (SpriteChar) de la entidad.
-	 * 
-	 * @return caracter de identificacion
-	 */
+	
 	public ElementTypes getElementType()
 	{
 		return type;
 	}
 
-	/**
-	 * Setea la representacion del elemento (SpriteChar) de la entidad.
-	 * 
-	 * @param spritechar
-	 */
+	
 	public void setElementType(ElementTypes type)
 	{
 		this.type = type;
 	}
 
-	/**
-	 * Devuelve el objeto posicion.
-	 * 
-	 * @return objeto posicion, (x,y)
-	 */
+	
 	public Position getPosition()
 	{
 		return pos;
 	}
 
-	/**
-	 * Setea el objeto posicion. (x,y)
-	 * 
-	 * @param pos
-	 */
 	public void setPosition(Position pos)
 	{
 		this.pos = pos;
 	}
 	
-	/**
-	 * Hace un comportamiento y borra el elemento.
-	 */
+	
 	public void die()
 	{
 		
 	}
 	
-	///////
 	
-	/**
-	 * Devuelve si el item es un diamante.
-	 * 
-	 * @return si el item es un diamante
-	 */
+	
+	
 	public boolean isDiamond()
 	{
 		if (this.getElementType() == ElementTypes.Diamond)
@@ -87,11 +57,7 @@ public class Element
 		}
 	}
 
-	/**
-	 * Devuelve si es una roca.
-	 * 
-	 * @return si es una roca
-	 */
+	
 	public boolean isRock()
 	{
 		if (this.getElementType() == ElementTypes.Rock)
@@ -104,11 +70,7 @@ public class Element
 		}
 	}
 	
-	/**
-	 * Devuelve si es vacio.
-	 * 
-	 * @return si es un bloque vacio
-	 */
+	
 	public boolean isEmpty()
 	{
 		if (this.getElementType() == ElementTypes.Empty)
@@ -121,10 +83,6 @@ public class Element
 		}
 	}
 	
-	/**
-	 * 
-	 * @return si la celda es tierra
-	 */
 	public boolean isDirt()
 	{
 		if (this.getElementType() == ElementTypes.Dirt)
@@ -137,10 +95,7 @@ public class Element
 		}
 	}
 
-	/**
-	 * 
-	 * @return si la celda es un muro.
-	 */
+	
 	public boolean isWall()
 	{
 		if (this.getElementType() == ElementTypes.Wall)
@@ -153,10 +108,7 @@ public class Element
 		}
 	}
 	
-	/**
-	 * 
-	 * @return si la celda es titanio
-	 */
+	
 	public boolean isTitanium()
 	{
 		if (this.getElementType() == ElementTypes.Titanium)
@@ -169,10 +121,7 @@ public class Element
 		}
 	}
 
-	/**
-	 * 
-	 * @return si la celda es la salida.
-	 */
+	
 	public boolean isExit()
 	{
 		if (this.getElementType() == ElementTypes.ExitOpen
@@ -186,11 +135,7 @@ public class Element
 		}
 	}
 	
-	/**
-	 * Determina si esta entidad es un actor.
-	 * 
-	 * @return si esta entidad es un actor
-	 */
+	
 	public boolean isActor()
 	{
 		if (this.getElementType().getKind().equals("Actor"))
@@ -203,11 +148,7 @@ public class Element
 		}
 	}
 
-	/**
-	 * Determina si esta entidad es Rockford.
-	 * 
-	 * @return si esta entidad es Rockford
-	 */
+	
 	public boolean isRockford()
 	{
 		if (this.getElementType() == ElementTypes.Rockford
@@ -224,11 +165,7 @@ public class Element
 		}
 	}
 
-	/**
-	 * Determina si esta entidad es un Item.
-	 * 
-	 * @return si esta entidad es un Item
-	 */
+	
 	public boolean isItem()
 	{
 		if (this.getElementType().getKind().equals("Item"))
